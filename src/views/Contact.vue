eee080fb4ffe7b9eef43a8d94cf8f6f602c733f1
nattakitseeam <nattakitseeam@gmail.com>
2026-03-11 02:50:06 +0700

    update 1103

---
+++ src/views/Contact.vue
@@ -0,0 +1,27 @@
+<template lang="">
+    <div class="container">
+        <div class="alert alert-primary" role="alert">
+  hellowwww
+</div>
+        <h1>show Contact</h1>
+        <h3>nattakit seeam</h3>
+        <p>Welcome to vue</p>    
+    </div>
+
+
+
+
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
+<style >
+h3{
+     color: #2c3e50;
+}
+    
+</style>
